240c9cf212740d394844b1104c2580df8fcd5c05
Tasbeeha Khan <tasbeehakhan05@gmail.com>
2025-12-11 00:21:17 +0500

    updated half

---
+++ weather.js
@@ -0,0 +1,37 @@
+async function getWeather() {
+    const country = document.getElementById("countryInput").value.trim();
+
+    if (!country) {
+        alert("Please enter a country name");
+        return;
+    }
+
+    const apiKey = "`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${apiKey}&units=metric`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (response.status === 401) {
+            document.getElementById("result").innerHTML = "❌ Invalid API Key!";
+            return;
+        }
+
+        if (data.cod == "404") {
+            document.getElementById("result").innerHTML = "❌ Country not found!";
+            return;
+        }
+
+        document.getElementById("result").innerHTML = `
+            <h3>${data.name}</h3>
+            <p>Temperature: ${data.main.temp}°C</p>
+            <p>Weather: ${data.weather[0].description}</p>
+            <p>Humidity: ${data.main.humidity}%</p>
+            <p>Wind Speed: ${data.wind.speed} m/s</p>
+        `;
+    } catch (error) {
+        console.log(error);
+        document.getElementById("result").innerHTML = "Network Error!";
+    }
+}
